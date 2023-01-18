f0548d7110ce82fb78213697322d95c5eaf3456d
umar260 <m.zafar9@share.epsb.ca>
2023-01-17 23:13:53 -0700

    4

--- main.py
+++ main.py
@@ -2,122 +2,123 @@ import re
 import json
 import os
 
+# student class defning student 
 class Student:
-    def __init__(self, name, classofs, dob, fees):
-        self.name = name
-        self.class_ = classofs
-        self.dob = dob
-        self.fees = fees
-        self.grades = {}
+    def __init__(self, name, grade, dateofbirth, fees):
+        self.name = name # name of student
+        self.grade = grade # grade of student 
+        self.dateofbirth = dateofbirth # date of birth of student 
+        self.fees = fees # fees of student
+        self.grades = {} # grades for the student used dictinary
 
+# list to storing the student data and 
+studentlist = []
 
-
-studentslist = []
-
-def loadfromjson():
+def jsondataload():
     try:
         with open("students.json", "r") as f:
-            data = json.load(f)
-            for student_data in data:
-                student = Student(student_data['name'], student_data['classofs'], 
-                                  student_data['dob'], student_data['fees'])
-                student.grades = student_data['grades']
-                studentslist.append(student)
-    except Exception as e:
-        print("Getting Error in loading json file: ",e)
+            jsonfile = json.load(f)
+            for eachstudent in jsonfile:
+                student = Student(eachstudent['name'], eachstudent['grade'], 
+                                  eachstudent['dateofbirth'], eachstudent['fees'])
+                student.grades = eachstudent['grades'] 
+                studentlist.append(student)# adding grades to the student
+    except Exception as e:# giving exception
+        print("Json file not opeing no jsonfile present: ",e)
 
-loadfromjson()        
+jsondataload()        
 
-def savetojson():
+def savejsondata():
     try:
         with open("students.json", "w") as f:
-            json.dump([student.__dict__ for student in studentslist], f)
+            json.dump([student.__dict__ for student in studentlist], f) # saving student data to json file
     except Exception as e:
-        print("Getting Error in saving the json file: ",e)
+        print("Error in saving json file: ",e)
 
                 
 
-def addstudents(name, classofs, dob, fees):
-    student = Student(name, classofs, dob, fees)
-    studentslist.append(student)
+def addstudent(name, grade, dateofbirth, fees):
+    student = Student(name, grade, dateofbirth, fees)
+    studentlist.append(student) # adding student to my list with append function
 
-
-def removestudents(name):
-    for i, student in enumerate(studentslist):
+# removing student from my list
+def removestudent(name):
+    for i, student in enumerate(studentlist):
         if student.name == name:
-            del studentslist[i]
-            print("Student is removed successfully!")
+            del studentlist[i]
+            print("Student removed successfully!")
             return
     print("Student not found.")
 
-def editstudents(name, newname=None, newclass=None, newdob=None, newfees=None):
-    for student in studentslist:
+# changing student details
+def editstudentdetails(name, newname=None, newclass=None, dateofbirth=None, fees=None):
+    for student in studentlist:
         if student.name == name:
             if newname:
                 student.name = newname
-            if newclass:
-                student.classofs = newclass
-            if newdob:
-                student.dob = newdob
-            if newfees:
-                student.fees = newfees
-            print("Student details are updated successfully!")
-            return
-    print("Student not found.")
 
-def viewstudents():
-    if not studentslist:
+# view every student data
+def viewstduents():
+    if not studentlist:
         print("No students found.")
         return
-    for student in studentslist:
+    for student in studentlist:
         print("Name:", student.name)
-        print("Class:", student.classofs)
-        print("Date of Birth:", student.dob)
+        print("Class:", student.grade)
+        print("Date of Birth:", student.dateofbirth)
         print("Fees:", student.fees)
-        print("-------------")    
+        print("-----------------------------") 
 
-def isvalidname(name):
+# checking name
+def validname(name):
     if re.match("^[a-zA-Z\s]*$", name):
         return True
     return False
 
-def isvalidclass(classofs):
-    if re.match("^[0-9a-zA-Z\s]*$", classofs):
+# checking grade
+def validclass(grade):
+    if re.match("^[0-9a-zA-Z\s]*$", grade):
         return True
     return False
 
-def isvaliddob(dob):
-    if re.match("^[0-9]{2}/[0-9]{2}/[0-9]{4}$", dob):
+
+# checking date of birth
+def validdateofbirth(dateofbirth):
+    if re.match("^[0-9]{2}/[0-9]{2}/[0-9]{4}$", dateofbirth):
         return True
     return False
 
-def isvalidfees(fees):
+# checking fees
+def validfees(fees):
     if re.match("^[0-9]*$", fees):
         return True
     return False  
 
+# adding grade for a student
 def addgrades(name, subject, grade):
-    for student in studentslist:
+    for student in studentlist:
         if student.name == name:
             student.grades[subject] = grade
-            print("Grades are added successfully!")
+            print("grade added in the student profile")
             return
-    print("Could not found the student.")
+    print("student not found")
 
+# editing grade for a student
 def editgrades(name, subject, new_grade):
-    for student in studentslist:
+    for student in studentlist:
         if student.name == name:
             if subject in student.grades:
                 student.grades[subject] = new_grade
-                print("Grades are updated successfully!")
+                print("grade added in the student profile")
                 return
             else:
                 print(f"Subject {subject} not found for student {name}")
                 return
-    print("Could not found the student.")
+    print("student not found")
 
+# viewing grade for a student
 def viewgrades(name):
-    for student in studentslist:
+    for student in studentlist:
         if student.name == name:
             if student.grades:
                 for subject, grade in student.grades.items():
@@ -125,71 +126,78 @@ def viewgrades(name):
             else:
                 print(f"No grades found for student {name}")
             return
-    print("Could not found the student.")                  
+    print("Student not found.")
 
 
-def menu():
+def menu():# main menu of the program
     while True:
-        print("1. Add a student")
-        print("2. Remove a student")
-        print("3. Edit student details")
-        print("4. View a students")
-        print("5. Add/Edit/View grades od the student")
-        print("6. To Exit")
-        selection = input("Enter your choice: ")
+        print("1 add student in database")
+        print("2 remove student in databse")
+        print("3 edit student details")
+        print("4 view every students")
+        print("5 grades")
+        print("6 exit")
+        choice = input("enter your choice: ")
 
-        if selection == "1":
-            name = input("Enter the student name: ")
-            classofs = input("Enter the class: ")
-            dob = input("Enter the date of birth of the student(dd/mm/yyyy): ")
-            fees = input("Enter the fees: ")
-            addstudents(name, classofs, dob, fees)
-            print("Student is added successfully!")
+        if choice == "1":
+            name = input("enter student name: ")
+            if name == "":
+                print("You cant left it empty")# checking if user left empty name
+                continue
+            grade = input("enter class: ")
+            if grade == "":
+                print("You cant left it empty")# checking if user left empty class
+                continue
+            dateofbirth = input("enter date of birth in the form of(dd/mm/yyyy): ")
+            if dateofbirth == "":
+                print("You cant left it empty")# checking if user left dob
+                continue
+            fees = input("enter fees: ")
+            if fees == "":
+                print("You cant left it empty")# checking if user left empty fees
+                continue
+            addstudent(name, grade, dateofbirth, fees)
+            print("student added succesfully in the list")
 
-        elif selection == "2":
-            name = input("Enter a student name: ")
-            removestudents(name)
 
-        elif selection == "3":
-            name = input("Enter a student name: ")
-            new_name = input("Enter a new name (Please Press Enter to skip): ")
-            new_class = input("Enter a new class (Please Press Enter to skip): ")
-            new_dob = input("Enter new date of birth of the student (PLease Press Enter to skip): ")
-            new_fees = input("Enter the new fees (PLease Press Enter to skip): ")
-            editstudents(name, new_name, new_class, new_dob, new_fees)
+        elif choice == "2":
+            name = input("Enter student name: ")
+            removestudent(name)
 
-        elif selection == "4":
-            viewstudents()
-
-        elif selection == "5":
-            print("1. To Add grades")
-            print("2. To Edit grades")
-            print("3. To View grades")
-            gradechoice = input("Enter your choice: ")
-
-            if gradechoice == "1":
-                name = input("Enter the student name: ")
-                subject = input("Enter the  subject: ")
-                grade = input("Enter the grade: ")
+        elif choice == "3":
+            name = input("Enter student name: ")
+            newname = input("Enter new name of the student ")
+            newclass = input("Enter new grade of the student ")
+            dateofbirth = input("Enter new date of birth ")
+            fees = input("Enter new fees of the student ")
+            editstudentdetails(name, newname, newclass, dateofbirth, fees)
+        elif choice == "4":
+            viewstduents()
+        elif choice == "5":
+            print("1 add grades of student")
+            print("2 edit grades of student")
+            print("3 view grades of student")
+            gradeselection = input("Enter your choice: ")
+            if gradeselection == "1":
+                name = input("Enter student name: ")
+                subject = input("Enter subject: ")
+                grade = input("Enter grade: ")
                 addgrades(name, subject, grade)
-
-            elif gradechoice == "2":
-                name = input("Enter the student name: ")
-                subject = input("Enter the subject: ")
-                new_grade = input("Enter the new grade: ")
+            elif gradeselection == "2":
+                name = input("Enter student name: ")
+                subject = input("Enter subject: ")
+                new_grade = input("Enter new grade: ")
                 editgrades(name, subject, new_grade)
-
-            elif gradechoice == "3":
-                name = input("Enter a student name: ")
+            elif gradeselection == "3":
+                name = input("Enter student name: ")
                 viewgrades(name)
             else:
-                print("Invalid choice. Can u Please try again.")
-
-        elif selection == "6":
-            savetojson()
+                print("wrong input please enter a valid input")
+        elif choice == "6":
+            savejsondata()
             print("Exiting...")
-            break 
+            break
         else:
-            print("Invalid choice. Can u Please try again.")
+            print("wrong input please enter a valid input")
 
 menu()
